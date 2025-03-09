3a9621a9c6a61a611f173ddc9008ee8b4f25e332
Monica Osuna <monicantosuna@gmail.com>
2025-03-08 21:01:33 -0500

    segundo commit

--- pos/src/main/java/com/rollerspeed/pos/Controller/PaginaController.java
+++ pos/src/main/java/com/rollerspeed/pos/Controller/PaginaController.java
@@ -1,20 +1,16 @@
 package com.rollerspeed.pos.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-
 
 @Controller
-
-
 public class PaginaController {
-    @GetMapping(value ="pagina")
-    public String mostrarPagina() {
-        return "pagina";
+
+    @GetMapping("/pagina")
+    public String mostrarPagina(Model model) {
+        model.addAttribute("mision", "La Escuela de Formación llamada RollerSpeed tiene como fin la formación integral de personas y deportistas, conectando positivamente al practicante con el patín como instrumento de transformación. Nuestros cursos están enfocados en fortalecer las bases del patinaje en cada practicante a través de sus distintos niveles formativos, consiguiendo que una vez superados estos niveles puedan iniciar su proceso especializado en alguna modalidad de patinaje.");
+        model.addAttribute("vision", "Posicionarse como la mejor escuela de patinaje, líder a nivel nacional e internacional posibilitando el desarrollo integral de todos nuestros deportistas entrenados a traves del tiempo mediante nuestros procesos deportivos.");
+        return "pagina"; // Debe coincidir con el nombre del archivo HTML en templates
     }
-    
 }
-    
-    
